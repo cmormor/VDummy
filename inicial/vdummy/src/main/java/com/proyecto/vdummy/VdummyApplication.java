ee60bf1be4b4e5b521255fe199f94f84c212fc99
Carmen Morales Moreno <151139927+cmormor@users.noreply.github.com>
2024-12-20 12:52:25 +0100

    Add files via upload

---
+++ inicial/vdummy/src/main/java/com/proyecto/vdummy/VdummyApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.vdummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdummyApplication.class, args);
+	}
+
+}
